fb66bb4a8adb122cf73ee126d06c6ae0f2801517
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-04-19 02:12:23 -0400

    Remove the timeout settings used during development in pseudo-distributed mode.

--- src/main/scripts/PageRank.sh
+++ src/main/scripts/PageRank.sh
@@ -5,10 +5,5 @@ hadoop jar page-rank.jar com.github.ygf.pagerank.PageRank \
     -D pagerank.damping_factor=0.85 \
     -D pagerank.max_iterations=2 \
     -D pagerank.top_results=100 \
-    -D yarn.am.liveness-monitor.expiry-interval-ms=3600000 \
-    -D yarn.nm.liveness-monitor.expiry-interval-ms=3600000 \
-    -D yarn.resourcemanager.container.liveness-monitor.interval-ms=3600000 \
-    -D mapreduce.jobtracker.expire.trackers.interval=3600000 \
-    -D mapreduce.task.timeout=3600000 \
     "$@"
 
