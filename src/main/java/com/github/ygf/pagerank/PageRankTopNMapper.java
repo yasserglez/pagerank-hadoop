f164ad18978da7bf4cbc4f2d2d55fd5da341d369
Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>
2014-04-20 00:47:55 -0400

    Add my email to the copyright notice.

--- src/main/java/com/github/ygf/pagerank/PageRankTopNMapper.java
+++ src/main/java/com/github/ygf/pagerank/PageRankTopNMapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Yasser Gonzalez Fernandez
+ * Copyright 2014 Yasser Gonzalez Fernandez <ygonzalezfernandez@gmail.com>.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
